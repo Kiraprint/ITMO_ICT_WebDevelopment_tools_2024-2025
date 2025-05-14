70f2a5354bdbfc75dcf580358cb02d87c740264d
Kiraprint <kirill.fdrn@yandex.ru>
2025-05-14 12:57:11 +0300

    alembic init

--- connection.py
+++ connection.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
 from sqlmodel import SQLModel, Session, create_engine
 
+load_dotenv()
 username = os.getenv("DB_USERNAME", "postgres")
 password = os.getenv("DB_PASSWORD", "")
 host = os.getenv("DB_HOST", "localhost")
